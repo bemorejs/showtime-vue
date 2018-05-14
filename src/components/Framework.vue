f606d69748c8200c2505f51c2a7a81428f78b43b
Ondrej Janosik <ondrej.janosik42@gmail.com>
2018-05-14 23:22:06 +0200

    console log added

--- src/components/Framework.vue
+++ src/components/Framework.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="card">
-    <img class="card-img-top logo"
-         :src="imageUrl"
-         alt="logo">
-    <div class="card-body">
-      <h5 class="card-title">{{ name }}</h5>
-      <p class="card-text">{{ name }} has {{ starsCount() }} of stars, {{ stars }} to be precise. If you want to update version, click on the button below</p>
-      <button type="button"
-              class="btn btn-primary"
-              @click="updateVersion">Update</button>
+    <div class="card">
+        <img class="card-img-top logo"
+             :src="imageUrl"
+             alt="logo">
+        <div class="card-body">
+            <h5 class="card-title">{{ name }}</h5>
+            <p class="card-text">{{ name }} has {{ starsCount() }} of stars, {{ stars }} to be precise. If you want to update version, click on the button below</p>
+            <button type="button"
+                    class="btn btn-primary"
+                    @click="updateVersion">Update</button>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Current version: {{ version }}</small>
+        </div>
     </div>
-    <div class="card-footer">
-      <small class="text-muted">Current version: {{ version }}</small>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -43,6 +43,7 @@ export default {
       this.$emit('update', this.name);
     },
     starsCount() {
+      console.log(`Calculating number of stars for ${this.name}`);
       if (this.stars > 50000) {
         return 'a lot';
       }
